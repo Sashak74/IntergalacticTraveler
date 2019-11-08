434a66ac9e2f91d473d65ff01d01c6c975aaa577
Sasha <okhabenyuk3212@stu.d214.org>
2019-11-08 11:26:11 -0600

    added new file and connected textfield

---
+++ IntergalacticTraveler/SecondViewContollerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewContollerViewController.swift
+//  IntergalacticTraveler
+//
+//  Created by Sasha Khabenyuk on 11/8/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewContollerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
